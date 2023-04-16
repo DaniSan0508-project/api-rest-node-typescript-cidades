17a44b03d641272914a5da7f2035edbe53f48db5
Daniel dos Santos C <danielsantos@sysfar.com.br>
2023-04-16 01:03:15 -0300

    feat: added yup for name validation

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import {StatusCodes} from 'http-status-codes';
 
+import { CidadesController } from '../controllers';
+
 const router = Router();
 
 router.get('/', (_, res)=> {
   return res.send('Olá DEV!');
 } );
 
-router.post('/teste', (req, res)=> {
-  return res.status(StatusCodes.PAYMENT_REQUIRED).json(req.body);
-} );
+router.post('/cidades', CidadesController.create);
 
 
 export { router };
